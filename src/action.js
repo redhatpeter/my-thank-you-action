998cea83466c2272826ac2ce4f44fe5bbef2ef12
Peter Lee <peterlee@M1-Max-Peters-MacBook-Pro.local>
2022-04-06 19:24:21 -0400

    saying thank you to new pull request

--- src/action.js
+++ src/action.js
@@ -1,4 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github')
+
 async function run() {
-    console.log('Hello, World!')
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const {context = {} } = github;
+    const {pull_request} = context.payload;
+   // console.log('Hello, World!')
+
+   await octokit.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you for submitting a pull request ! we will try to review as soon as we can'
+  });
 }
 run();
